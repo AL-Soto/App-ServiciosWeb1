1f0beb916335a0572795c58a6d02e7d43f454dc4
Alexander Soto <soto1214a@hotmail.com>
2023-09-16 20:55:59 -0500

    create CommissionEmployee

--- EmployeeOOP/EmployeeOOP/Class/CommissionEmployee.cs
+++ EmployeeOOP/EmployeeOOP/Class/CommissionEmployee.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace EmployeeOOP.Class
 {
-    internal class CommissionEmployee
+    public class CommissionEmployee : Employee
     {
+        #region Properties
+        public decimal commisionPercentaje { get; set; }
+        public int sales { get; set; }
+        #endregion
+
+        #region Methods
+        public CommissionEmployee()
+        {
+
+        }
+        public override decimal GetValueToPay()
+        {
+            return commisionPercentaje * sales;
+        }
+        public override string ToString()
+        {
+            return $"{base.ToString()}" +
+                $"Porcentaje de comision: {commisionPercentaje}" +
+                $"Numero de ventas: {sales}"
+                ;
+        }
+        #endregion
     }
 }
