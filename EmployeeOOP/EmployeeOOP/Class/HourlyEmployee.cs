b4b904450599a29ac08b8b7991b2724655edbcd2
Alexander Soto <soto1214a@hotmail.com>
2023-09-16 20:49:19 -0500

    Create HourlyEmployee

---
+++ EmployeeOOP/EmployeeOOP/Class/HourlyEmployee.cs
@@ -0,0 +1,36 @@
+﻿namespace EmployeeOOP.Class
+{
+    public class HourlyEmployee : Employee
+    {
+        #region Properties
+
+        public int hours { get; set; }
+        public decimal hoursValue { get; set; }
+
+
+        #endregion
+
+        #region Methods 
+
+        public HourlyEmployee()
+        {
+
+        }
+        public override decimal GetValueToPay()
+        {
+            return hoursValue * hours;
+        }
+
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}" +
+                $"Horas: {hours}" +
+                $"Valor por Hora: {hoursValue}"
+                ;
+        }
+
+        #endregion
+
+    }
+}
