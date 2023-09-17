33f20c36959d7b98f1ffa1d86f2df2e3ed21c14d
Alexander Soto <soto1214a@hotmail.com>
2023-09-16 21:11:55 -0500

    create BaseCommissionEmploye an update Comission

--- EmployeeOOP/EmployeeOOP/Class/CommissionEmployee.cs
+++ EmployeeOOP/EmployeeOOP/Class/CommissionEmployee.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EmployeeOOP.Class
+﻿namespace EmployeeOOP.Class
 {
     public class CommissionEmployee : Employee
     {
